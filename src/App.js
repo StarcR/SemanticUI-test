99bcc0e395c7ccb941a8728212925963b21b4264
StarcR <starc.rudi@gmail.com>
2021-04-22 12:48:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import DropdownExample from './DropdownExample';
+import ButtonExample from './ButtonExample';
+import FormExampleSubmitText from './FormExpampleSubmitText';
+
+function App() {
+  return (
+  <div >
+    <h2>Hello</h2> 
+    <ButtonExample />
+    <br></br>
+    <FormExampleSubmitText />
+    <br></br>
+    <DropdownExample />
+  </div>
+  );
+}
+
+export default App;
